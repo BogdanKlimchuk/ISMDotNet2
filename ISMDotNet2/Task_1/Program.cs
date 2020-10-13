640b7a8df4af9fc01728a9b05477b4f80166378d
Bogdan <ipz202_kbs@student.ztu.edu.ua>
2020-10-13 12:19:48 +0300

    Commit1

---
+++ ISMDotNet2/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, P, S;
+            Console.WriteLine("A = ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("B = ");
+            b = int.Parse(Console.ReadLine());
+            P = 2 * a + 2 * b;
+            S = a * b;
+            Console.WriteLine($"P = {P}");
+            Console.WriteLine($"S = {S}");
+        }
+    }
+}
